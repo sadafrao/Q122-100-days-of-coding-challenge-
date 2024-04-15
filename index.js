4dfdefe5abb25054cec26ae8d86a87cac36e001c
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-15 17:34:26 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// Q122
+// this is a while loop to count down 10 to 1 and break the loop when it reaches 5
+var i = 10;
+while (i > 0) {
+    console.log(i);
+    i--;
+    if (i === 5) {
+        break;
+    }
+}
